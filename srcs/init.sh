2ed528adcbdcbed21a491131b350454b8a210559
sserbin <stanleyserbin@gmail.com>
2021-04-03 04:55:35 +0200

    STASH 14: Mysql ok

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,3 +1,5 @@
 /etc/init.d/php7.3-fpm start
 /etc/init.d/nginx start
+/etc/init.d/mysql start
+echo "create database wordpress; create user wordpressuser; set password for wordpressuser = PASSWORD('user42'); GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost IDENTIFIED by 'user42';" | mysql
 sleep infinity
